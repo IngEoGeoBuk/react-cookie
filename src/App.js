ea4e22d890c35184c0bfbd3b914e4dbff42318b6
SeongWoo Kang <you3667@nepirity.com>
2021-07-27 13:14:49 +0900

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react"; 
+import { useCookies } from "react-cookie"; 
+function App() { 
+  const [text, setText] = useState(''); 
+  const [isRemember, setIsRemember] = useState(false); 
+  const [cookies, setCookie, removeCookie] = useCookies(['rememberText']); 
+  
+  let now = new Date; 
+  let after1m = new Date(); 
+  
+  useEffect(() => { 
+    if (cookies.rememberText !== undefined) { 
+      setText(cookies.rememberText); 
+      setIsRemember(true); 
+    } 
+  }, []); 
+  
+  function onChange(e) { 
+    setText(e.target.value); 
+  } 
+  
+  const handleOnChange = () => { 
+    after1m.setMinutes(now.getMinutes() + 1); 
+    setIsRemember(true); 
+    setCookie('rememberText', text, { path: '/', expires: after1m });
+    window.location.reload();
+  } 
 
-function App() {
+  const removeremove = () => {
+    removeCookie('rememberText');
+    window.location.reload();
+  }
+    
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+      <> 
+        <input value={text} onChange={onChange} /> 
+        <button onClick={() => { handleOnChange() }}>아이디 저장해보자</button>
+        <button onClick={() => { removeremove() }}>아이디 지워보자</button>
+        {/* <input type="checkBox" onChange={handleOnChange} checked={isRemember} />  */}
+        <h1>{text}</h1> 
+      </>
+    ); 
+  }
 
 export default App;
